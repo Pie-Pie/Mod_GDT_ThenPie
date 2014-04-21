932254599917e4a1feae7f321913cdf388ddf234
Morgan PERRE <morgan.perre@gmail.com>
2014-04-21 18:02:05 +0100

    MoreWorkers mod init files
    Signed-off-by: Morgan PERRE <morgan.perre@gmail.com>

---
+++ mods/moreworkers/inside/moreworkers.js
@@ -0,0 +1,48 @@
+
+// Sorte de class
+var MoreWorkers = {};
+(function () {
+	// donnée membre
+	var workers = [[]]; // Genre workers[idEmploy][data] ==> data peu donner nbWorkersInTeam, EmployEfiencienty, others...
+	
+	
+	
+	MoreWokers.start = function () {
+		console.info("Started !");
+		
+		
+		
+		
+	}
+	
+	
+	
+	/////////////////////////////////////////////// FUNCTIONS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/////////////////////////////////////////// EVENTS DEFINITION \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	///////////////////////////////////////////// OTHER THINGS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	
+	
+	
+})();
