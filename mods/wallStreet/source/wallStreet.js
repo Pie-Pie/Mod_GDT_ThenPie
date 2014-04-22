afd415837a82f2730783fdda12e45d5204c914ce
Alexandre Rabérin <kernelith@live.fr>
2014-04-22 23:37:57 +0100

    We can now hire a trader

--- mods/wallStreet/source/wallStreet.js
+++ mods/wallStreet/source/wallStreet.js
@@ -25,15 +25,99 @@ if (typeof String.prototype.dlocalize === "undefined" || (String.prototype.dloca
 
 (function()
 {
+	var m_storedDatas;
+	var m_haveTrader = false;
+	
+	var saveData = function() 
+	{
+		// Save mod datas
+		m_storedDatas.data["haveTrader"] = m_haveTrader;
+	}
+	
+	var loadData = function() 
+	{
+		m_storedDatas = GDT.getDataStore("wallStreet");
+		
+		// Load mod datas
+		m_haveTrader = m_storedDatas.data["haveTrader"];
+	}
+	
 	wallStreet.init = function()
 	{
-		/*var a = $(".simplemodal-data").find(".characterPreviewCanvas")[0];
-        alert(GameManager);
-		//a.height = 254;
-        /*a.width = 360;
-        a = a.getContext("2d");
-        a.drawImage("../image/desk.png", 0, 0, 360, 254);*/
+		// Event to save and load mod datas
+		GDT.on(GDT.eventKeys.saves.saving, saveData);
+		GDT.on(GDT.eventKeys.saves.loading, loadData);
 		
+		// Event to Hire a Trader (Generate a dialog window)
+		var hireTraderEvent =
+		{
+			id: "8FFA272-EF44-44B0-B6F2-9EEE69957996",
+			
+			trigger: function()
+			{
+				return false;
+			},
+			
+			getNotification: function()
+			{
+				return new Notification(
+										{
+											sourceId: "8FFA272-EF44-44B0-B6F2-9EEE69957996",
+											header: "Hire Trader".dlocalize(),
+											text: "Do you want to hire a Trader ?".dlocalize(),
+											options: ["Yes".dlocalize(), "No".dlocalize()]
+										});
+			},
+			
+			complete: function(result)
+			{
+				if(result != 0) // Case not answer Yes
+					return;
+				
+				var notif = new Notification(
+											{
+												header: "Trader Hired".dlocalize(),
+												text: "You have successfully hire a Trader.\n\nYou can now train him and allocate a budget that he can use to do some speculations!".dlocalize(),
+											});
+				
+				m_haveTrader = true;
+				
+				GameManager.company.notifications.push(notif);
+			}
+		};
+		GDT.addEvent(hireTraderEvent);
 		
+		// Add if necessary an item to context menu to hire a trader (when click on PDG)
+		var baseContextMenu = UI.showContextMenu;	// Recover actual function on the show context Menu
+		var hireTrader = function(items, pos)
+		{
+			// Recover which character has been clicked
+			var selectedCharacter = UI.getCharUnderCursor();
+			// Check if the PDG as been clicked
+			var pdgSelected = (selectedCharacter && selectedCharacter == GameManager.company.staff[0]);
+			
+			// Check if the player is at lvl 4 with no trader hired
+			if (pdgSelected && GameManager.company.currentLevel >= 4 && !m_haveTrader)
+			{			
+				// Create Item for the context menu
+				var hireTraderItem =
+				{
+					label: "Hire a Trader".dlocalize(),
+					action: function()
+					{
+						Sound.click();
+						GameManager.company.notifications.insertAt(0, hireTraderEvent.getNotification());
+						GameManager.resume(true);
+					}
+				};
+				// Insert hireTraderItem at first pos in context menu
+				items.splice(0, 0, hireTraderItem);
+			}
+
+			// Call to the old function to keep the same behaviour
+			baseContextMenu(items, pos);
+		};
+		UI.showContextMenu = hireTrader;	// Put our custom function on show context Menu	
 	};
+	
 })();
