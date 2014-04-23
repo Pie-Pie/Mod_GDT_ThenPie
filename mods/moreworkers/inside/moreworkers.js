36d18acf5f04700d7a5736834d141b0a251c2950
Morgan PERRE <morgan.perre@gmail.com>
2014-04-23 15:08:53 +0100

    + Add data storage
    ~ Flemme ~

--- mods/moreworkers/inside/moreworkers.js
+++ mods/moreworkers/inside/moreworkers.js
@@ -2,10 +2,21 @@
 // Sorte de class
 var MoreWorkers = {};
 (function () {
-	// donnée membre
-	var workers = [[]]; // Genre workers[idEmploy][data] ==> data peu donner nbWorkersInTeam, EmployEfiencienty, others...
+
+	var modID = "moreworkers";
+	
+	var m_dataStore;
 	
 	
+	var load = function () {
+		if(!m_dataStore)
+			m_dataStore = GDT.getDataStore(modID);
+
+			
+		if (!m_dataStore.data.workers)
+			m_dataStore.data.workers = [[]]; // Genre workers[idEmploy][data] ==> data peu donner nbWorkersInTeam, EmployEfiencienty, others...
+			
+	}
 	
 	MoreWokers.start = function () {
 		console.info("Started !");
